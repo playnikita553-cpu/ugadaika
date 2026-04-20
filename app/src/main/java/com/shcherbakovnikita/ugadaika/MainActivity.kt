b086533013b83c1a541a3c00edaa2a3bfc65fa51
Иван Петров <playnikita553@gmail.com>
2026-04-20 22:17:13 +0300

    new

--- app/src/main/java/com/shcherbakovnikita/ugadaika/MainActivity.kt
+++ app/src/main/java/com/shcherbakovnikita/ugadaika/MainActivity.kt
@@ -3,9 +3,13 @@ package com.shcherbakovnikita.ugadaika
 import android.graphics.drawable.Drawable
 import android.media.Image
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -27,24 +31,23 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setSupportActionBar(binding.toolbar)
     startGame()
 
     binding.proverka.setOnClickListener()
     {
 
-        if((popitki != 0) and (binding.proverka.text.toString() != "") and (binding.textResult.text != "Победа:]") )
-        {
+
         proverkaChisel()
-        }
+
     }
 
     binding.newGame.setOnClickListener()
     {
         startGame()
+        binding.proverka.visibility = View.VISIBLE
     }
 
 
@@ -57,48 +60,70 @@ class MainActivity : AppCompatActivity()
 
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(R.id.settings == item.itemId){
+            Toast.makeText(this, "settings", Toast.LENGTH_SHORT)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+
+
+
+
+
 
     private fun randomChislo(): Int = Random.nextInt(min, max)
 
     private fun startGame()
     {
-    binding.textResult.setText("")
-        var vvod: EditText = binding.vvodd
+    binding.textResult.text = ""
         popitki = maxPopitki
-        vvod.text.toString()
-        vvod.setHint("Введите число от $min, до $max")
-        binding.textResult.setText("")
-        binding.popitki.setText("Попыток: $popitki")
+        binding.vvodd.text.toString()
+        binding.vvodd.hint = "Введите число от $min, до $max"
+        binding.textResult.text = ""
+        binding.popitki.text = "Попыток: $popitki"
         c = randomChislo()
         binding.imagee.visibility = View.INVISIBLE
         binding.newGame.visibility = View.INVISIBLE
 
     }
-    private fun proverkaChisel()
-    {
+    private fun proverkaChisel() {
+        val userNumber = getUserNumber()
 
-       if ((binding.vvodd.text.toString().toInt() == c))
-       {
-           win()
-       }
-       else
-           if(binding.vvodd.text.toString().toInt() < c) binding.textResult.setText("Нужно число больше")
 
-           else
-               if(binding.vvodd.text.toString().toInt() > c) binding.textResult.setText("Нужно число меньше")
+        if (userNumber == -1) {
+            binding.textResult.setText("Введите число!")
+            binding.vvodd.setText("")
+            return
+        }
+
+        if (userNumber == c) {
+            win()
+        }
+        else
+            if (userNumber < c) {
+            binding.textResult.setText("Нужно число больше")
+        }
+        else
+            if (userNumber > c) {
+            binding.textResult.setText("Нужно число меньше")
+        }
 
         popitki--
+        binding.popitki.text = "Осталось попыток: $popitki"
 
-
-        binding.popitki.setText("Осталось попыток: $popitki")
-
-
-        if(popitki <= 0) fail()
-
-
+        if (popitki <= 0) fail()
 
         binding.vvodd.setText("")
-
     }
 
     private fun win()
@@ -108,10 +133,10 @@ class MainActivity : AppCompatActivity()
 
         binding.newGame.visibility = View.VISIBLE
 
-        binding.textResult.setText("Победа:]")
+        binding.textResult.text = "Победа:]"
 
         binding.imagee.setImageResource(R.drawable.win)
-
+        binding.proverka.visibility = View.INVISIBLE
 
 
 
@@ -123,13 +148,23 @@ class MainActivity : AppCompatActivity()
         binding.newGame.visibility = View.VISIBLE
 
 
-        binding.textResult.setText("Поражение:[ (правильное число было: ${randomChislo()})")
+        binding.textResult.text = "Поражение:[ (правильное число было: ${c})"
 
         binding.imagee.setImageResource(R.drawable.lose)
-
+        binding.proverka.visibility = View.INVISIBLE
 
     }
 
+    private  fun getUserNumber(): Int{
+        var a: Int
+        try {
+            a = binding.vvodd.text.toString().toInt()
+        }
+        catch (e: Exception){
+            a = -1
+        }
+        return a
+    }
 
     }
 
